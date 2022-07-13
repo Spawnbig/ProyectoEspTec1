0bb376ca35d555a2063b8cfdf5561a4e6709fd1b
Rafael Ignacio Mella San Martin <r.mella06@ufromail.cl>
2022-07-13 18:46:33 -0400

    Se agrega funciones al dashboard y se agrega el Json con los datos de usuario

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -6,45 +6,41 @@
 <script>
 import DatosUsuario from '../components/DatosUsuario.vue'
 import Grafico from '../components/Grafico.vue'
+//import axios from 'axios'
+import usuariosDB from '../Json/usuarios.json'
+
 export default {
   components:{DatosUsuario,Grafico},
   data(){
     return{
       dataUser: '',
       vistaGrafico: false,
+      username:''
     }
   },
   created(){
     // Llamar a la API
-    this.dataUser = {
-        id:1,
-        username:'zmorrott0',
-        sessions:[
-          {id:1,date:'02/11/2021',score:'3.9',duration:'60'},
-          {id:2,date:"09/14/2021",score:'1.6',duration:'13'},
-          {id:3,date:'10/12/2021',score:'4.1',duration:'47'},
-          {id:4,date:'03/10/2021',score:'1.2',duration:'32'},
-          {id:5,date:'02/11/2021',score:'3.9',duration:'60'},
-          {id:6,date:'02/11/2021',score:'3.9',duration:'60'},
-          {id:7,date:'02/11/2021',score:'3.9',duration:'60'},
-          {id:8,date:'02/11/2021',score:'3.9',duration:'60'},
-          {id:9,date:'03/10/2021',score:'1.2',duration:'32'},
-          {id:10,date:'02/11/2021',score:'3.9',duration:'60'},
-          {id:11,date:'02/11/2021',score:'3.9',duration:'60'},
-          {id:12,date:'02/11/2021',score:'3.9',duration:'60'},                                 
-        ]
-      }
+    //let url = "https://7qak3a37b4dh7kisebhllubdxq0dnehm.lambda-url.us-east-1.on.aws/"
+    this.username = JSON.parse(localStorage.getItem('user')).username
+    this.obtenerDatos(usuariosDB)
+    //console.log(userName.username);
+
+    /*axios.get(url).then(response => {
+     console.log(response.data);
+    });*/
+   
   },
   methods:{
     cambiarVista(){
       this.vistaGrafico = (this.vistaGrafico) ? false : true;
+    },
+    obtenerDatos(db){
+      db.forEach(e => {      
+        if (e.username == this.username){
+          this.dataUser = e;
+        }
+      });
     }
   }
 }
 </script>
-
-<style scoped>
-
-
-
-</style>
